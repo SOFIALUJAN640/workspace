5272910d4b518337049c0550a6c0eb65200c5b20
SOFIALUJAN640 <SOFIALUJANA@IELACANDELARIAMEDELLIN.EDU.CO>
2025-08-28 18:33:44 -0500

    ejercicios de If del 28 de agosto

---
+++ ejercicioif.js
@@ -0,0 +1,21 @@
+function positivo(){
+    const numero = document.getElementById("numero");
+    const numeroInt = parseInt(numero.value);
+
+    if(numeroInt >=0){
+        alert("el numero es positivo")
+    }else{
+        alert("el numero es negativo")
+    }
+}
+
+function espar(){
+    const numero = document.getElementById("par");
+    const numeroInt = parseInt(numero.value);
+
+    if(numeroInt %2==0){
+        alert("el numero es par")
+    }else{
+        alert("el numero es impar")
+    }
+}
